33e14b935a197e901ba175792534c482245b3b48
陈帅 <1301491593@qq.com>
2019-01-17 16:09:57 +0800

    '123'

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="detail">
     <div v-wechat-title="$route.meta.title = bookTitle"></div>
-    <div class="content">
-      <div class="back" @click="back">
-        <img src="../assets/back.png" alt="back">
-      </div>
-      <iframe :src="url" frameborder="0" style="width: 100%;height: 100%"></iframe>
-    </div>
   </div>
 </template>
 
@@ -30,6 +24,7 @@ export default {
         this.bookTitle = res.info.book
         this.bookTag = res.info.bookTag
         this.url = res.info.url
+        window.location.href = res.info.url
       } else {
         console.log(res.info)
       }
